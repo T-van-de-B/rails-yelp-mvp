4d80a45cdb53b12024467bf6896fdf598fef5ecc
Thomas Britz <britzthomas@gmail.com>
2019-08-16 10:53:08 +0100

    d

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ Restaurant.create(
+    name: 'A',
+    address: 'street A',
+    phone_number: '21',
+    category: 'belgian'
+  )
+
+ Restaurant.create(
+    name: 'B',
+    address: 'street B',
+    phone_number: '21',
+    category: 'chinese'
+  )
+
+ Restaurant.create(
+    name: 'C',
+    address: 'street C',
+    phone_number: '21',
+    category: 'italian'
+  )
+
+ Restaurant.create(
+    name: 'D',
+    address: 'street D',
+    phone_number: '21',
+    category: 'japanese'
+  )
+
+ Restaurant.create(
+    name: 'E',
+    address: 'street E',
+    phone_number: '21',
+    category: 'french'
+  )
+
